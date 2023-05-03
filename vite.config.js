62760e272e23885dbe492d1afcf8439edf70c52e
Oliver-M <oliver1417@163.com>
2023-05-04 01:01:26 +0800

    2023-5-4-LW-1

--- vite.config.js
+++ vite.config.js
@@ -22,7 +22,7 @@ export default defineConfig({
     proxy: { // 本地开发环境通过代理实现跨域
       // 正则表达式写法
       '/api': {
-        target: 'http://localhost:8081', // 后端服务实际地址
+        target: 'http://localhost:8000', // 后端服务实际地址
         changeOrigin: true, //开启代理
         rewrite: (path) => path.replace(/^\/api/, '')
       }
